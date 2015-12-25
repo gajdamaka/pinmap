f4e84d402c76f7a88efa62fd22f67be1221f9d96
Sergii Bondarenko <broken@propeople.com.ua>
2015-12-25 16:31:38 +0200

    Reorganized JS: added autocomplete search, setting center by bounds etc.

--- files/ctools--pin-map--google-map--content-type--default.js
+++ files/ctools--pin-map--google-map--content-type--default.js
@@ -1,112 +1,148 @@
-(function($, animationSpeed) {
+/**
+ * @namespace PinMap.GoogleMap.ContentType
+ */
+(function($) {
   'use strict';
 
-  Drupal.behaviors.pinMap = {
-    attach: function(context, settings) {
-      settings = settings.PinMap.GoogleMap.ContentType;
+  /**
+   * An array of settings for every pane.
+   *
+   * @type {Array}
+   */
+  var settings = Drupal.settings.PinMap.GoogleMap.ContentType;
 
-      var $containers = $(context).find('.pin-map-area'),
-          $searchForm = $containers.find('form'),
-          $searchInput = $searchForm.find('input'),
-          postalCodes = {},
-          apiQuery = function(address, callback) {
-            $.getJSON(encodeURI(settings.apiUrl + address), function(response) {
-              if (response.hasOwnProperty('status') && response.status.toLowerCase() === 'ok') {
-                callback(response);
-              }
-            });
-          };
-
-      // Collect postal codes.
-      $.each(settings.pins, function() {
-        postalCodes[this.postal_code] = this.postal_code;
-      });
-
-      $containers.find('.map').each(function() {
-        var mapContainer = this,
-            $message = $(mapContainer).css('height', settings.map.height).children('.not-found'),
-            map = new google.maps.Map(mapContainer, {
-              mapTypeControl: false,
-              scrollwheel: false,
-              noClear: true,
-              styles: JSON.parse(settings.map.styles),
-              zoom: parseInt(settings.map.zoom, 10)
-            });
-
-        // Set map center.
-        $searchInput.bind('search', (function search() {
-          map.setCenter(settings.map.region.geometry.location);
-
-          return search;
-        })());
-
-        $message.css({
-          marginLeft: -($message.width() / 2),
-          marginTop: -($message.height() / 2)
-        }).bind('click', function() {
-          // Hide error message.
-          $message.animate({top: '100%', opacity: 0}, animationSpeed, function() {
-            $message.css('top', 0);
-          });
-        });
-
-        $searchForm.bind('submit', function(event) {
-          event.preventDefault();
-
-          var postalCode = $searchInput.val();
-
-          if ('' !== postalCode) {
-            if (postalCodes.hasOwnProperty(postalCode)) {
-              apiQuery([postalCode, settings.map.region.formatted_address].join(', '), function(response) {
-                map.setCenter(response.results[0].geometry.location);
-              });
-            }
-            else {
-              // Slide error message from top.
-              $message.animate({top: '50%', opacity: 1}, animationSpeed);
-            }
-          }
-        });
-
-        $.each(settings.pins, function() {
-          var infoWindow = new google.maps.InfoWindow(),
-              marker = new google.maps.Marker(),
-              place = this.data.locality,
-              html = '';
-
-          html += '<strong>' + this.organisation_name + '</strong>';
-          html += '<br>';
-          html += place.formatted_address;
-          html += '<br>';
-          html += Drupal.t('Phone: @phone', {'@phone': this.phone_number});
-          html += '<br>';
-
-          if (this.data.information) {
-            html += Drupal.t('Information: @info', {'@phone': this.data.information});
-          }
-
-          infoWindow.setContent(html);
-
-          marker.setMap(map);
-          // Set the position of the marker using the place ID and location.
+  $('.pin-map-area').each(function(i) {
+    var options = settings[i],
+        $container = $(this),
+        $searchForm = $container.find('form'),
+        $searchInput = $searchForm.find('input'),
+        $mapContainer = $container.find('.map'),
+        autocomplete = new google.maps.places.Autocomplete($searchInput[0]),
+        map = new google.maps.Map($mapContainer[0], {
+          mapTypeControl: false,
+          scrollwheel: false,
+          styles: JSON.parse(options.map.styles)
+        }),
+        placeMarker = new google.maps.Marker({
+          map: map
+        }),
+        setMarkerPlace = function(marker, place) {
           marker.setPlace({
             placeId: place.place_id,
             location: place.geometry.location
           });
 
-          if (settings.icon.hasOwnProperty('url')) {
-            marker.setIcon(settings.icon.url);
+          return marker;
+        },
+        createInfoWindow = function(info) {
+          var infoWindow = new google.maps.InfoWindow(),
+              html = [];
+
+          if (info.organisation_name) {
+            html.push('<strong>' + info.organisation_name + '</strong>');
           }
 
-          if (google.maps.Animation.hasOwnProperty(settings.icon.animation)) {
-            marker.setAnimation(google.maps.Animation[settings.icon.animation]);
+          html.push(info.data.locality.formatted_address);
+
+          if (info.phone_number) {
+            html.push(Drupal.t('Phone: @phone', {'@phone': info.phone_number}));
           }
 
-          marker.addListener('click', function() {
-            infoWindow.open(map, this);
-          });
-        });
+          if (info.data.information) {
+            html.push(Drupal.t('Information: @info', {'@phone': info.data.information}));
+          }
+
+          infoWindow.setContent(html.join('<br>'));
+
+          return infoWindow;
+        },
+        setMapCenter = (function setMapCenter() {
+          var bounds = new google.maps.LatLngBounds(),
+              place = autocomplete.getPlace(),
+              markers = options.markers.length;
+
+          // Place will exist only if "place_changed" event has been
+          // triggered on a search field. This condition is necessary
+          // because this function will be triggered on window resizing
+          // and map initializing.
+          if (undefined !== place) {
+            // Extend bounds by found address.
+            bounds.extend(place.geometry.location);
+
+            // Change styles for marker of the desired place
+            // to make it easy to distinguish from the rest.
+            placeMarker.setIcon({
+              path: 'M27.648 -41.399q0 -3.816 -2.7 -6.516t-6.516 -2.7 -6.516 2.7 -2.7 6.516 2.7 6.516 6.516 2.7 6.516 -2.7 2.7 -6.516zm9.216 0q0 3.924 -1.188 6.444l-13.104 27.864q-0.576 1.188 -1.71 1.872t-2.43 0.684 -2.43 -0.684 -1.674 -1.872l-13.14 -27.864q-1.188 -2.52 -1.188 -6.444 0 -7.632 5.4 -13.032t13.032 -5.4 13.032 5.4 5.4 13.032z',
+              scale: .6,
+              fillColor: 'green',
+              strokeColor: 'black',
+              fillOpacity: .85,
+              strokeWeight: .2,
+              strokeOpacity: 1
+            });
+
+            // Place marker of found address on the map.
+            setMarkerPlace(placeMarker, place);
+            // Increase markers number because we set
+            // a new one to mark search location.
+            markers++;
+          }
+
+          // Bounds will be set only if we have many markers. Otherwise
+          // zoom level will be too big.
+          if (markers > 1) {
+            $.each(options.markers, function() {
+              bounds.extend(new google.maps.LatLng(this.data.locality.geometry.location));
+            });
+
+            map.fitBounds(bounds);
+          }
+          else {
+            // Here we will have only one marker on the map and should center
+            // the map using it coordinates.
+            map.setCenter(options.markers[0].data.locality.geometry.location);
+            map.setZoom(11);
+          }
+
+          return setMapCenter;
+        })();
+
+    $mapContainer.css('height', options.map.height);
+
+    if (options.search.disabled) {
+      $searchForm.hide();
+    }
+    else {
+      $searchForm.bind('submit', function(event) {
+        event.preventDefault();
+        google.maps.event.trigger(autocomplete, 'place_changed');
       });
     }
-  };
-})(jQuery, 500);
+
+    $.each(options.markers, function() {
+      var infoWindow = createInfoWindow(this),
+          marker = new google.maps.Marker();
+
+      marker.setMap(map);
+
+      if (options.icon.url) {
+        marker.setIcon(options.icon.url);
+      }
+
+      if (google.maps.Animation.hasOwnProperty(options.icon.animation)) {
+        marker.setAnimation(google.maps.Animation[options.icon.animation]);
+      }
+
+      setMarkerPlace(marker, this.data.locality).addListener('click', function() {
+        infoWindow.open(map, this);
+      });
+    });
+
+    $.each({
+      resize: window,
+      place_changed: autocomplete
+    }, function(event, object) {
+      google.maps.event.addDomListener(object, event, setMapCenter);
+    });
+  });
+})(jQuery);
