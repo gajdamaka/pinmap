6da8360a01f15b75672b73228e4af6df1e0fca06
Sergii Bondarenko <broken@propeople.com.ua>
2016-01-20 17:10:20 +0200

    Added zero before "."

--- pinmap.js
+++ pinmap.js
@@ -181,11 +181,11 @@
         placeMarker.setIcon({
           // @todo Use an image file here.
           path: 'M27.648 -41.399q0 -3.816 -2.7 -6.516t-6.516 -2.7 -6.516 2.7 -2.7 6.516 2.7 6.516 6.516 2.7 6.516 -2.7 2.7 -6.516zm9.216 0q0 3.924 -1.188 6.444l-13.104 27.864q-0.576 1.188 -1.71 1.872t-2.43 0.684 -2.43 -0.684 -1.674 -1.872l-13.14 -27.864q-1.188 -2.52 -1.188 -6.444 0 -7.632 5.4 -13.032t13.032 -5.4 13.032 5.4 5.4 13.032z',
-          scale: .6,
+          scale: 0.6,
           fillColor: 'green',
           strokeColor: 'black',
-          fillOpacity: .85,
-          strokeWeight: .2,
+          fillOpacity: 0.85,
+          strokeWeight: 0.2,
           strokeOpacity: 1
         });
 
